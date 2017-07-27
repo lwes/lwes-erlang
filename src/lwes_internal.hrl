ea6e10a15dc91bc713fbce730a6d5349fe96260f
Anthony Molinaro <anthonym@alumni.caltech.edu>
2017-07-27 23:18:07 +0000

    Moved some defines which should have be private.

--- src/lwes_internal.hrl
+++ src/lwes_internal.hrl
@@ -26,7 +26,6 @@
           element (4,V) >= 0 andalso
           element (4,V) =< 255)).
 
-
 -record (lwes_channel, {ip,
                         port,
                         is_multicast,
@@ -36,5 +35,42 @@
                         ref
                        }).
 -record (lwes_multi_emitter, {type, m, n}).
+
+-define (LWES_TYPE_U_INT_16,            1).
+-define (LWES_TYPE_INT_16,              2).
+-define (LWES_TYPE_U_INT_32,            3).
+-define (LWES_TYPE_INT_32,              4).
+-define (LWES_TYPE_STRING,              5).
+-define (LWES_TYPE_IP_ADDR,             6).
+-define (LWES_TYPE_INT_64,              7).
+-define (LWES_TYPE_U_INT_64,            8).
+-define (LWES_TYPE_BOOLEAN,             9).
+-define (LWES_TYPE_BYTE,               10).
+-define (LWES_TYPE_FLOAT,              11).
+-define (LWES_TYPE_DOUBLE,             12).
+-define (LWES_TYPE_LONG_STRING,        13).
+-define (LWES_TYPE_U_INT_16_ARRAY,    129).
+-define (LWES_TYPE_INT_16_ARRAY,      130).
+-define (LWES_TYPE_U_INT_32_ARRAY,    131).
+-define (LWES_TYPE_INT_32_ARRAY,      132).
+-define (LWES_TYPE_STRING_ARRAY,      133).
+-define (LWES_TYPE_IP_ADDR_ARRAY,     134).
+-define (LWES_TYPE_INT_64_ARRAY,      135).
+-define (LWES_TYPE_U_INT_64_ARRAY,    136).
+-define (LWES_TYPE_BOOLEAN_ARRAY,     137).
+-define (LWES_TYPE_BYTE_ARRAY,        138).
+-define (LWES_TYPE_FLOAT_ARRAY,       139).
+-define (LWES_TYPE_DOUBLE_ARRAY,      140).
+-define (LWES_TYPE_N_U_INT_16_ARRAY,  141).
+-define (LWES_TYPE_N_INT_16_ARRAY,    142).
+-define (LWES_TYPE_N_U_INT_32_ARRAY,  143).
+-define (LWES_TYPE_N_INT_32_ARRAY,    144).
+-define (LWES_TYPE_N_STRING_ARRAY,    145).
+-define (LWES_TYPE_N_INT_64_ARRAY,    147).
+-define (LWES_TYPE_N_U_INT_64_ARRAY,  148).
+-define (LWES_TYPE_N_BOOLEAN_ARRAY,   149).
+-define (LWES_TYPE_N_BYTE_ARRAY,      150).
+-define (LWES_TYPE_N_FLOAT_ARRAY,     151).
+-define (LWES_TYPE_N_DOUBLE_ARRAY,    152).
 
 -endif.
